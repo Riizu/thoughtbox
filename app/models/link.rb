bf816102d0b1f6db07225c2d229d0cadc80acdc2
riizu <rgbatty@outlook.com>
2016-09-11 12:29:17 -0600

    add user relation to link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ApplicationRecord
   validates :url, url: true, presence: true
   validates :title, presence: true
+
+  belongs_to :user
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -3,21 +3,25 @@ require 'rails_helper'
 RSpec.describe Link, type: :model do
   it { should validate_presence_of(:url) }
   it { should validate_presence_of(:title) }
+  it { should belong_to(:user) }
 
   it "validates urls" do 
-    link = Link.new(url: "http://www.testlink.com", title: "Test link")
+    user = create(:user)
+    link = Link.new(url: "http://www.google.com", title: "Test link", user: user)
 
     expect(link.valid?).to eq true
   end
 
   it "invalidates urls" do 
-    link = Link.new(url: "httf://www.testlink.com", title: "Test link")
+    user = create(:user)
+    link = Link.new(url: "httf://www.testlink.com", title: "Test link", user: user)
 
     expect(link.valid?).to eq false
   end
 
   it "has a default read value of false" do
-    link = Link.new(url: "http://www.testlink.com", title: "Test link")
+    user = create(:user)
+    link = Link.new(url: "http://www.testlink.com", title: "Test link", user: user)
 
     expect(link.read).to eq false
   end
